281714714f207ab8ab892ccb7ebe9230635d8c83
Khachatur Shahinyan <khachatur.shahinyan@everymatrix.com>
2024-05-31 14:35:50 +0400

    lint with black

--- app.py
+++ app.py
@@ -4,23 +4,27 @@ from flask_healthz import healthz
 
 app = Flask(__name__)
 
-@app.route('/liveness')
+
+@app.route("/liveness")
 def liveness():
-  time.sleep(2);
-  return "<h1><center>Liveness check completed</center><h1>"
-  
-@app.route('/readiness')
+    time.sleep(2)
+    return "<h1><center>Liveness check completed</center><h1>"
+
+
+@app.route("/readiness")
 def readiness():
-  try:
-    time.sleep(10);
-    return "<h1><center>Readiness check completed</center><h1>"
-  except Exception:
-    raise HealthError("Readiness failed")
+    try:
+        time.sleep(10)
+        return "<h1><center>Readiness check completed</center><h1>"
+    except Exception:
+        raise HealthError("Readiness failed")
+
 
 @app.route("/")
 def hello():
-  return "<h1><center>Hello World app! Version 1</center><h1>"
+    return "<h1><center>Hello World app! Version 1</center><h1>"
+
 
 if __name__ == "__main__":
 
-  app.run(host='0.0.0.0',port=5000)
+    app.run(host="0.0.0.0", port=5000)
