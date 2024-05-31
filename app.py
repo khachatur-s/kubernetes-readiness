d0934ab8546591491178e43ddb3b3694f5ba12c7
Khachatur Shahinyan <khachatur.shahinyan@everymatrix.com>
2024-05-31 12:19:28 +0400

    add app and Dockerfile

---
+++ app.py
@@ -0,0 +1,26 @@
+import time
+from flask import Flask
+from flask_healthz import healthz
+
+app = Flask(__name__)
+
+@app.route('/liveness')
+def liveness():
+  time.sleep(2);
+  return "<h1><center>Liveness check completed</center><h1>"
+  
+@app.route('/readiness')
+def readiness():
+  try:
+    time.sleep(10);
+    return "<h1><center>Readiness check completed</center><h1>"
+  except Exception:
+    raise HealthError("Readiness failed")
+
+@app.route("/")
+def hello():
+  return "<h1><center>Hello World app! Version 1</center><h1>"
+
+if __name__ == "__main__":
+
+  app.run(host='0.0.0.0',port=5000)
